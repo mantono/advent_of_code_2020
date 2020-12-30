a7979b270a3f1c141590961e4c22ae17af8d49a8
Anton Österberg <anton.osterberg@gmail.com>
2020-12-30 23:11:00 +0100

    Some clean up for day 10

--- mantono-rust/src/days/day10.rs
+++ mantono-rust/src/days/day10.rs
@@ -23,8 +23,7 @@ pub fn first(input: String) -> String {
     adapters
         .iter()
         .zip(others)
-        .inspect(|(a0, a1)| println!("{} - {}", a1, a0))
-        .map(|(a0, a1)| dbg!(a1 - *a0))
+        .map(|(a0, a1)| a1 - *a0)
         .for_each(|n| match n {
             1 => d1 += 1,
             3 => d3 += 1,
@@ -62,7 +61,6 @@ fn optional(adapters: &[usize]) -> bool {
 }
 
 fn summarize(options: Vec<bool>) -> usize {
-    println!("{:?}", options);
     let mut opt_state: usize = 0;
     let mut trib_sums: Vec<usize> = Vec::with_capacity(options.len() / 4);
     for optional in options {
@@ -85,22 +83,6 @@ fn trib(n: usize) -> usize {
     }
 }
 
-/* fn count_options(adapters: &[usize], current: usize, index: usize) -> usize {
-    if adapters.is_empty() {
-        return 1;
-    }
-    //std::thread::sleep(std::time::Duration::from_millis(10));
-
-    adapters
-        .iter()
-        .take(3)
-        .enumerate()
-        .inspect(|(i, next)| println!("n: {}", next))
-        .filter(|(_, next)| **next - current <= 3)
-        .map(|(i, next)| count_options(&adapters[i + 1..], *next, i) + 1)
-        .sum()
-} */
-
 #[cfg(test)]
 mod tests {
     use super::second;
@@ -121,97 +103,8 @@ mod tests {
 4";
         // Input:    (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
         //            R   R  R  O  O  R   R   O   R   R   R   R   R
-        //            trib(3 + 2) * trib(3 + 1) =
+        //            trib(2 + 2) * trib(2 + 1) =
         //            4 * 2 = 8
-
-        // Input:    (0), 1, [4, 5, 6, 7], [10, 11, 12], 15, 16, 19, (22)
         assert_eq!("8", &second(input.to_string()));
     }
 }
-
-// [0, 3] => 1
-//  R  R
-//  trib(3 + 0) = 1
-
-// [0, 1, 3] => 2
-//  R  O  R
-// trib(3 + 1) => 2
-// ---------
-// [0, 3]
-// [0, 1, 3]
-//
-
-// 3 * 2 * 1 * 2 * 1 * 1 =
-// 3 * 2 * 2 = 12
-// 12 / 3 = 4
-// [0, 1, 2, 3] => 4
-//  3  2  1
-//     1  2  3
-//  -  O  O  R
-// trib(3 + 2) => 4
-// -----------
-// [0, 1, 2, 3]
-// [0, 1, 3]
-// [0, 2, 3]
-// [0, 3]
-
-// 3! * 2! * 2! =
-// 3 * 2 * 2 * 2 = 24
-// 24 / 4 = 6
-// [0, 1, 2, 3, 5] => 6
-//  3, 2, 2, 1
-//     1  2  3  2
-// -   O  O  R  R
-// --------------
-// [0, 1, 2, 3, 5]
-// [0, 1, 2, 5]
-// [0, 1, 3, 5]
-// [0, 2, 3, 5]
-// [0, 2, 5]
-// [0, 3, 5]
-
-// [0, 1, 2, 3, 6] => 4
-//  3  2  1  1
-//     1  2  3  1!
-//  -  O  O  R  R  => 2^2 = 4
-// --------------
-// [0, 1, 2, 3, 6]
-// [0, 1, 3, 6]
-// [0, 2, 3, 6]
-// [0, 3, 6]
-
-// [0, 3, 6, 9, 10] => 1
-// ---------------
-// [0, 3, 6, 9, 10]
-//  1, 1, 1, 1, 1
-
-// (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22) => 8
-//  1   1  3  2  1  1  2   1   1   1   1   1
-//  -   R  R  O  O  O  R   O   R   R   R   R    R => 2^3 = 8
-// 7
-
-// [0, 1] => 1
-//  R  R
-
-// [0, 1, 2] => 2
-//  R  O  R
-
-// [0, 1, 2, 3] => 4
-//  R  O  O  R
-// [1, 2]
-// [1]
-// [2]
-// []
-
-//  [0, 1, 2, 3, 4] => 7
-//   3  3  2  1
-//      1  2  3  3
-//   -  O  O  O  R
-// OOO => 7
-// 1 2 3
-// 1 2
-// 1 3
-// 2 3
-// 1
-// 2
-// 3
